dc74bffb41fc9b3a149cdcabc86c879e14199734
unija28 <cabellunesa@gmail.com>
2025-05-21 18:19:30 +0800

    Create Quiz1.js

---
+++ Quiz1.js
@@ -0,0 +1,12 @@
+//Using a CONDITIONAL Statement, Alert() and Prompt()
+let user_password = "lunesa041312";
+let user_name = "Lunesa";
+let password = prompt("What is your user_password:");
+let user_Name = prompt("What is your user_name:");
+
+if(user_password == password){
+    console.log(`Welcome ${user_name})
+} else{
+
+    console.log(Maybe Username or Password is Invalid or Does not Match`)
+}
