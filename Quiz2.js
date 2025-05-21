5648876f12377bc701413ebb9c8003686615269f
unija28 <cabellunesa@gmail.com>
2025-05-21 18:20:44 +0800

    Create Quiz2.js

---
+++ Quiz2.js
@@ -0,0 +1,11 @@
+//Using a LOOP STATEMENT and prompt()
+
+
+let dogBreed = prompt("Dog breed:");
+
+let printCount = prompt("Print Count:");
+
+
+for(let i=0; i < printCount; i++){
+    console.log(dogBreed);
+}
